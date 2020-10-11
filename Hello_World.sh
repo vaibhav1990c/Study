e12fc95079b09086d2f65ee9a027eb3fdca5bd36
vaibhav chandna (MOB-E403) <vaibhav.chandna@digispice.com>
2020-10-11 13:13:56 +0530

    ifelseif

--- Hello_World.sh
+++ Hello_World.sh
@@ -1,10 +1,16 @@
 echo "Hello User!!"
-echo "Please share Your Name With us:"
+echo "Please Enter the total %age :"
 read var1
-if [[ ! $var1 == "Trixy" ]]
+if [[ $var1 > 60 ]]
 then
-echo "Hello Master $var1!!"
+echo "You Passed with First Divison"
+elif [[ $var1 > 40 ]]
+then
+echo "You Passed with Second Division"
+elif [[ $var1 > 33 ]]
+then
+echo "You have just passed"
 else
-echo "Hello $var1"
+echo "Invalid Entry"
 fi
 
