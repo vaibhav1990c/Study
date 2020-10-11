110344c0e2c773306339d1aba07e2ec142d8065e
vaibhav chandna (MOB-E403) <vaibhav.chandna@digispice.com>
2020-10-11 13:02:46 +0530

    ifelse

--- Hello_World.sh
+++ Hello_World.sh
@@ -1,8 +1,10 @@
 echo "Hello User!!"
 echo "Please share Your Name With us:"
 read var1
-if [[ $var1 == "Trixy" ]]
+if [[ ! $var1 == "Trixy" ]]
 then
-echo "Hello $var1!!"
+echo "Hello Master $var1!!"
+else
+echo "Hello $var1"
 fi
 
